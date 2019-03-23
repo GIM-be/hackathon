c968f30615e894086a60b446efd421733dc550dc
romainh <romain.hardenne@gim.be>
2019-03-23 07:46:54 +0100

    Create user and notification tables

--- backend/database/z_init.sql
+++ backend/database/z_init.sql
@@ -19,3 +19,26 @@ create table proposal
   description text,
   CONSTRAINT parcel_srid_geometry CHECK ((st_srid(geometry) = 3857))
 );
+
+CREATE INDEX proposal_geometry_idx ON proposal USING gist (geometry);
+
+
+CREATE TABLE "user"
+(
+    id serial PRIMARY KEY,
+    login varchar(64) NOT NULL
+);
+CREATE UNIQUE INDEX user_login_uindex ON "user" (login);
+
+CREATE TABLE notification_zone
+(
+    id serial PRIMARY KEY,
+    id_user integer not null,
+    geometry geometry,
+    name varchar(64) NOT NULL,
+    CONSTRAINT notification_zone_id_user_fk FOREIGN KEY (id_user) REFERENCES "user"(id),
+    CONSTRAINT parcel_srid_geometry CHECK ((st_srid(geometry) = 3857))
+);
+
+create index notification_zone_id_user_index on notification_zone (id_user);
+CREATE INDEX notification_zone_geometry_idx ON notification_zone USING gist (geometry);
